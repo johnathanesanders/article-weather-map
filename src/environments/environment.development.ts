2e9c6aa33adafcfe61a25d527b85d2025ebc76b0
Johnathan Sanders <johnathanesanders@outlook.com>
2023-07-18 07:07:56 -0500

    update default center to Liberal, Kansas

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     leaflet: {
-        defaultCenter: [32.888140, -97.356768],
+        defaultCenter: [37.0430812, -100.920999],
     },
     production: false,
     serviceWorker: false
